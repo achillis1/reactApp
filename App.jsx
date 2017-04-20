063daf5a53e60071aa1341dfd3a3652eaffb6969
Ding Li <dli@nexant.com>
2017-04-20 14:02:17 -0500

    add some html markups in jsx

--- App.jsx
+++ App.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 class App extends React.Component {
    render() {
+        var i = 1;
+
+        var myStyle = {
+           fontSize: 100,
+           color: '#FF0000'
+        }
+
       return (
          <div>
             <h1>Header</h1>
             <h2>Content</h2>
             <p data-myattribute = "somevalue">This is the content!!!</p>
+            <h1>{1+1}</h1>
+            <h1>{i == 1 ? 'True!' : 'False'}</h1>
+            <h1 style = {myStyle}>Header</h1>
+            {//End of the line Comment...}
+            {/*Multi line comment...*/}
          </div>
       );
    }
